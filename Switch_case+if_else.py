f9457d55e72aeb909aa3b270f61288df5c0af77f
George <faso.312@yandex.ru>
2024-12-09 18:07:44 +0400

    edit sict_value logic | 18:07 9_12_24

--- Switch_case+if_else.py
+++ Switch_case+if_else.py
@@ -1,27 +1,24 @@
+
+
 # Пример использования if-else
 def check_number(num):
-    if num > 0:
-        return "Положительное число"
-    elif num < 0:
-        return "Отрицательное число"
-    else:
-        return "Ноль"
+    if num > 0: return "Положительное число"
+    elif num < 0: return "Отрицательное число"
+    else: return "Ноль"
 
 print(check_number(5))
 print(check_number(-3))
 print(check_number(0))
 
 # Пример использования switch-case с помощью словаря
-def switch_case(value):
-    switcher = {
-        1: "Первый",
-        2: "Второй",
-        3: "Третий"
-    }
-    return switcher.get(value, "Неизвестное значение")
 
-print(switch_case(1))
-print(switch_case(4))
+switcher = {1: "Первый", 2: "Второй", 3: "Третий"}
+
+def switch_case(dict,value):
+    return dict.get(value, "Неизвестное значение")
+
+print(switch_case(switcher,1))
+print(switch_case(switcher,4))
 
 # Пример работы со словарями исключений
 def get_color_name(color_code):
@@ -35,14 +32,10 @@ def get_color_name(color_code):
 print(get_color_name("R"))
 print(get_color_name("Y"))
 
-# Пример использования if-else с списками
 def categorize_fruits(fruit):
-    if fruit in ["яблоко", "груша", "персик"]:
-        return "Косточковые"
-    elif fruit in ["банан", "апельсин"]:
-        return "Цитрусовые"
-    else:
-        return "Неизвестный фрукт"
+    if fruit in ["яблоко", "груша", "персик"]: return "Косточковые"
+    elif fruit in ["банан", "апельсин"]: return "Цитрусовые"
+    else: return "Неизвестный фрукт"
 
 print(categorize_fruits("яблоко"))
 print(categorize_fruits("банан"))
